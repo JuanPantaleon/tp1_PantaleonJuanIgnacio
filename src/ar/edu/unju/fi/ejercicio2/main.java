8b9e6e97a0b2099487493a48ad54c03e1bbc4163
PC <PC@Panta>
2024-04-04 14:51:39 -0300

    Ejercicio 2

---
+++ src/ar/edu/unju/fi/ejercicio2/main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		int edadPersona = 23;
+		float alturaEdificio = 150;
+		float precioProducto = 1500;
+	//  long numeroTelefono = 3885074830 ;
+		String numeroTelefono = "3885074830";
+		double calculoCoseno = Math.cos(0.5);
+		
+		System.out.println("El nombre del pais es: "+nombrePais);
+		System.out.println("La edad de la persona es: "+ edadPersona);
+		System.out.println("La altura del edificio es: "+ alturaEdificio + " kilometros");
+		System.out.println("El precio del producto del supermercado es: " + precioProducto + " Pesos");
+		System.out.println("El numero de telefono es: "+ numeroTelefono);
+		System.out.println("El coseno de 0.5 es: "+ calculoCoseno);
+	
+	}
+
+}
