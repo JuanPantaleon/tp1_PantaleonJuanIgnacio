55c1e5377e4ac3aed41bb78fc6bd673c203d5bc3
PC <PC@Panta>
2024-04-04 14:45:13 -0300

    Inicio del Tp1

---
+++ src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio1;
+public class main {
+
+	public static void main(String[] args) {
+		float primerNumero = 8;
+		float segundoNumero = 5;
+		float tercerNumero = 1;
+		float cuartoNumero = 20;
+		float quintoNumero = 9;
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println("El promedio es: "+ promedio );
+		
+	}
+
+}
