3d5c4fec0002c75476596f4b0f0ed25fd058e806
PC <PC@Panta>
2024-04-04 15:16:18 -0300

    Ejercicio 1 v2

--- src/ar/edu/unju/fi/ejercicio1/main.java
+++ src/ar/edu/unju/fi/ejercicio1/main.java
@@ -2,12 +2,14 @@ package ar.edu.unju.fi.ejercicio1;
 public class main {
 
 	public static void main(String[] args) {
-		float primerNumero = 8;
-		float segundoNumero = 5;
-		float tercerNumero = 1;
-		float cuartoNumero = 20;
-		float quintoNumero = 9;
-		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		int primerNumero = 8;
+		int segundoNumero = 5;
+		int tercerNumero = 1;
+		int cuartoNumero = 20;
+		int quintoNumero = 9;
+		double promedio = (double)(primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		
+		System.out.println(primerNumero +", "+ segundoNumero +", "+ tercerNumero +", "+ cuartoNumero +", "+ quintoNumero);
 		System.out.println("El promedio es: "+ promedio );
 		
 	}
